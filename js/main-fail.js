ccd1df3ec7001009e4ac43af4bb79a55becb6a2e
Oh JuYeon <dpffpself@naver.com>
2023-06-13 16:57:23 +0900

    2023-06-13 하 뭔소리고 이게

--- js/main-fail.js
+++ js/main-fail.js
@@ -7,32 +7,6 @@ class PolyRhythm{
         this.angle = 0;
         this.toLeft = true;
         this.stop = false;
-
-        this.gradients = [
-            "#d0e7f5",
-            "#d9e7f4",
-            "#d6e3f4",
-            "#bcdff5",
-            "#b7d9f4",
-            "#c3d4f0",
-            "#9dc1f3",
-            "#9aa9f4",
-            "#8d83ef",
-            "#ae69f0",
-            "#d46ff1",
-            "#db5ae7",
-            "#d911da",
-            "#d601cb",
-            "#e713bf",
-            "#f23cae",
-            "#fb79ab",
-            "#ffb6c1",
-            "#fed2cf",
-            "#fddfd5",
-            "#fedcd1",
-        ];
-
-        this.startTime = new Date().getTime();
     }//constructor
 
     init(){
@@ -76,20 +50,21 @@ class PolyRhythm{
         /* draw */
         this.draw();
 
+        /* angle */
+        if(this.toLeft){
+            this.angle--;
+            if(this.angle < -180) this.toLeft = false;
+        }else{
+            this.angle++;
+            if(this.angle > 0) this.toLeft = true;
+        }
+
         requestAnimationFrame(this.polyrhythm);
     }//polyrhythm
 
 
     draw = () =>{
         const {xStart,xEnd,xCenter,y} = this.pos;
-
-        /* bottom line */
-        this.ctx.strokeStyle = "#ffffff";
-        this.ctx.lineWidth = 6;
-        this.ctx.beginPath();
-        this.ctx.moveTo(xStart, y);
-        this.ctx.lineTo(xEnd, y);
-        this.ctx.stroke();
         
         /* circles */
         const radius = (xEnd - xStart) / 2;
@@ -100,17 +75,24 @@ class PolyRhythm{
                 x : xCenter, 
                 y : y, 
                 radius : radius - (per * i),
-                angle : this.angle,
-                idx : i
+                angle : this.angle
             });
         }//for
+
+        /* bottom line */
+        this.ctx.strokeStyle = "#ffffff";
+        this.ctx.lineWidth = 6;
+        this.ctx.beginPath();
+        this.ctx.moveTo(xStart, y);
+        this.ctx.lineTo(xEnd, y);
+        this.ctx.stroke();
     }//draw
 
     draw_arc = (OBJ = {}) => {
-        const {x,y,radius,angle,idx} = OBJ;
+        const {x,y,radius,angle} = OBJ;
         const deg = (Math.PI / 180);
         this.ctx.beginPath();
-        this.ctx.strokeStyle = this.gradients[idx];
+        this.ctx.strokeStyle = "#aaaaaa";
         this.ctx.lineWidth = 1;
         this.ctx.arc(
             x,y, 
@@ -122,28 +104,20 @@ class PolyRhythm{
         this.draw_point({
             angle : angle,
             parentRadius : radius,
-            idx : idx
         });
     }//draw_arc
 
     draw_point(OBJ = {}){
-        const {idx,parentRadius} = OBJ;
-        const elapsedTime = new Date().getTime() - this.startTime;
-        const v = (2 * Math.PI)  / 5000;
-        const velocity = v + (idx * 0.0001);
-        const maxAngle = 2 * Math.PI;
-        const distance = Math.PI + elapsedTime * velocity;
-        const modDistance = distance % maxAngle; 
-        const adjustDistance = modDistance >= Math.PI ? modDistance : maxAngle - modDistance;
-
         const {xCenter, y:parentY} = this.pos;
-        const x = Math.cos(adjustDistance) * parentRadius + xCenter;
-        const y = Math.sin(adjustDistance) * parentRadius + parentY;
+        const {angle,parentRadius} = OBJ;
+        const rad = angle * (Math.PI / 180);
+        const x = Math.cos(rad) * parentRadius + xCenter;
+        const y = Math.sin(rad) * parentRadius + parentY;
         
         this.ctx.beginPath();
-        this.ctx.fillStyle = this.gradients[idx];
+        this.ctx.fillStyle = "#ffffff";
         const radius = window.innerWidth * 0.8 / 21 / 6;
-        this.ctx.arc(x,y, radius ,0,Math.PI * 2);
+        this.ctx.arc(x,y - 6, radius ,0,Math.PI * 2);
         this.ctx.fill();
     }//draw_point
 }//class-PolyRhythm
